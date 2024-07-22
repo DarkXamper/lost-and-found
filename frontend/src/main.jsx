1095e14ef5f7e04bd9b07aac7d62409694bcfa8b
pantha704 <prathamjaiswal204@gmail.com>
2024-07-22 20:12:53 +0530

    added google oauth

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./router/router.jsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+const GOOGLE_CLIENT_ID =
+  "710428268267-mbt8dqh2ng3gkocdrvtq2jcto1djh3jn.apps.googleusercontent.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-    {/* <App /> */}
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <RouterProvider router={router} />
+      {/* <App /> */}
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
